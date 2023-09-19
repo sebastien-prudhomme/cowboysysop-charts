357b7d6ef86799562f250ad78e735510b0e5d0c1
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2023-09-19 22:14:36 +0200

    WIP

---
+++ generator/generator-all.sh
@@ -0,0 +1,13 @@
+python generator.py applications/dolibarr.yaml
+python generator.py applications/flowise.yaml
+python generator.py applications/kroki.yaml
+python generator.py applications/kubebox.yaml
+python generator.py applications/kubeview.yaml
+python generator.py applications/lighthouse-ci.yaml
+python generator.py applications/local-ai.yaml
+python generator.py applications/mongo-express.yaml
+python generator.py applications/n8n.yaml
+python generator.py applications/qdrant.yaml
+python generator.py applications/quickchart.yaml
+python generator.py applications/vertical-pod-autoscaler.yaml
+python generator.py applications/whoami.yaml
