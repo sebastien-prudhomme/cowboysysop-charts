6950114908c3e61cbaa01641a5eeb83c1ac06d4d
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2023-10-04 16:57:34 +0200

    WIP

--- charts/local-ai/test-stablediffusion.sh
+++ charts/local-ai/test-stablediffusion.sh
@@ -1,7 +1,6 @@
 LOCALAI=http://local-ai.internal
 
 curl $LOCALAI/v1/images/generations -H "Content-Type: application/json" -d '{
-            "prompt": "floating hair, portrait, ((loli)), ((one girl)), cute face, hidden hands, asymmetrical bangs, beautiful detailed eyes, eye shadow, hair ornament, ribbons, bowties, buttons, pleated skirt, (((masterpiece))), ((best quality)), colorful|((part of the head)), ((((mutated hands and fingers)))), deformed, blurry, bad anatomy, disfigured, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, long body, Octane renderer, lowres, bad anatomy, bad hands, text",
-            "mode": 2,  "seed":9000,
-            "size": "256x256", "n":1
+  "prompt": "A cute baby sea otter",
+  "size": "256x256"
 }'
