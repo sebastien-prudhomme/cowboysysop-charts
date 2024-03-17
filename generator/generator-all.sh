50249b087c2454f06d31d2599c0dd2abe30a3f0b
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2024-03-17 11:10:40 +0100

    WIP

--- generator/generator-all.sh
+++ generator/generator-all.sh
@@ -7,6 +7,7 @@ python3 generator.py applications/lighthouse-ci.yaml
 python3 generator.py applications/local-ai.yaml
 python3 generator.py applications/mongo-express.yaml
 python3 generator.py applications/n8n.yaml
+python3 generator.py applications/ollama.yaml
 python3 generator.py applications/qdrant.yaml
 python3 generator.py applications/quickchart.yaml
 python3 generator.py applications/vertical-pod-autoscaler.yaml
